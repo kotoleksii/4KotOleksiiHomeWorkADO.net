1c5632ddcb0373bdaaf847f4882ad8e02fb06d5f
Oleksii Kot <akkot08@gmail.com>
2020-12-11 13:23:33 +0200

    Add project files.

---
+++ 20201209_db_data_adapter/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _20201209_db_data_adapter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(12, 12);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.Size = new System.Drawing.Size(541, 253);
+            this.dgvMain.TabIndex = 0;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtQuery.Location = new System.Drawing.Point(13, 272);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(371, 110);
+            this.txtQuery.TabIndex = 1;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(395, 272);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 40);
+            this.btnGet.TabIndex = 2;
+            this.btnGet.Text = "Get data";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(479, 272);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 40);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save data";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 395);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.dgvMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ 20201209_db_data_adapter/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace _20201209_db_data_adapter
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        DataSet set = null;
+        SqlDataAdapter adapter = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string connString = ConfigurationManager
+                .ConnectionStrings["localDbCS"]
+                .ConnectionString;
+
+            connection = new SqlConnection(connString);
+
+            set = new DataSet();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectQuery = txtQuery.Text;
+
+                adapter = new SqlDataAdapter(selectQuery, connection);
+                SqlCommandBuilder cb = new SqlCommandBuilder(adapter);
+
+                //modifyUpdateCommand();
+
+                modifyUpdateCommandByProcedure();
+
+                set.Tables.Clear();
+                adapter.Fill(set, "buf_table");                
+                dgvMain.DataSource = set.Tables["buf_table"];
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"ERROR: {ex.Message}");
+            }
+        }
+
+        private void modifyUpdateCommand()
+        {
+            string query = "update users set nickname = @p_nickname, password = @p_password where id = @p_id";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.Add(new SqlParameter("@p_nickname", SqlDbType.NVarChar)
+            {
+                SourceVersion = DataRowVersion.Current, 
+                SourceColumn = "nickname"
+            });
+
+            cmd.Parameters.Add(new SqlParameter("@p_password", SqlDbType.NVarChar)
+            {
+                SourceVersion = DataRowVersion.Current,
+                SourceColumn = "password"
+            });
+
+            cmd.Parameters.Add(new SqlParameter("@p_id", SqlDbType.Int)
+            {
+                SourceVersion = DataRowVersion.Original,
+                SourceColumn = "id"
+            });
+
+            adapter.UpdateCommand = cmd;
+        }
+
+
+        private void modifyUpdateCommandByProcedure()
+        {
+            SqlCommand cmd = new SqlCommand("updateUsersAdapterCommand", connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.Add("@nickname", SqlDbType.NVarChar, 50, "nickname");
+            cmd.Parameters.Add("@password", SqlDbType.NVarChar, 50, "password");
+            cmd.Parameters.Add("@id", SqlDbType.Int, 0, "id")
+                .SourceVersion = DataRowVersion.Original;
+
+            adapter.UpdateCommand = cmd;
+
+        }
+
+        private void modifySelectCommandByProcedure()
+        {
+            SqlCommand cmd = new SqlCommand("selectUsersAdapterCommand", connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.Add("@email", SqlDbType.NVarChar, 50);
+            cmd.Parameters.Add("@nickname", SqlDbType.NVarChar, 50);
+
+            adapter.SelectCommand = cmd;
+        }
+
+        private void modifyInsertCommandByProcedure()
+        {
+            SqlCommand cmd = new SqlCommand("insertUsersAdapterCommand", connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.Add("@email", SqlDbType.NVarChar, 50, "email");
+            cmd.Parameters.Add("@nickname", SqlDbType.NVarChar, 50, "nickname");
+            cmd.Parameters.Add("@password", SqlDbType.NVarChar, 50, "password");
+
+            adapter.InsertCommand = cmd;
+        }
+
+        private void modifyDeleteCommandByProcedure()
+        {
+            SqlCommand cmd = new SqlCommand("deleteUsersAdapterCommand", connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.Add("@id", SqlDbType.Int, 0, "id")
+                .SourceVersion = DataRowVersion.Original;
+
+            adapter.DeleteCommand = cmd;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //adapter.Update(set, "buf_table");
+                DataTable table = set.Tables["buf_table"];
+                adapter.Update(table.Select(null, null, DataViewRowState.Deleted));
+                adapter.Update(table.Select(null, null, DataViewRowState.ModifiedCurrent));
+                adapter.Update(table.Select(null, null, DataViewRowState.Added));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"ERROR: {ex.Message}");
+            }
+        }
+    }
+}
